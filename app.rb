c5a5d6573cb159e91d688dd49504ced15642e987
AlexDresco <alexandra.d1510@gmail.com>
2018-09-17 15:30:44 +0100

    Created views folder and cleaned the index file'

--- app.rb
+++ app.rb
@@ -14,7 +14,6 @@ get '/welcome' do
 end
 
 get '/cat' do
-  "<div style='border: 5px dashed red'>
-    <img src='http://bit.ly/1eze8aE'>
-  </div>"
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
 end
